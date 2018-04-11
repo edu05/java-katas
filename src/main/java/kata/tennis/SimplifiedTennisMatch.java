1f33d8f38bb3fa712436fdd4d269de60c31b89c3
eduardowt <jewt_5@hotmail.com>
2018-04-11 20:24:32 +0100

    written code for second AC

--- src/main/java/kata/tennis/SimplifiedTennisMatch.java
+++ src/main/java/kata/tennis/SimplifiedTennisMatch.java
@@ -5,19 +5,27 @@ public class SimplifiedTennisMatch {
     private final String player1;
     private final String player2;
 
+    private int player1Points = 0;
+    private int player2Points = 0;
+
     public SimplifiedTennisMatch(String player1, String player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
     public void score(String playerName) {
+        if (playerName.equals(player1)) {
+            player1Points++;
+        } else if (playerName.equals(player2)) {
+            player2Points++;
+        }
 
     }
 
     public String formatScore() {
         String score = new StringBuilder()
                 .append(player1 + " - " + player2 + "\n")
-                .append("0 - 0")
+                .append(player1Points + " - " + player2Points)
                 .toString();
         return score;
     }
