4fbe78411a50f73230ec301562ba5b4a99643bd2
eduardowt <jewt_5@hotmail.com>
2018-04-11 19:55:29 +0100

    written the code for the third AC

--- src/main/java/kata/bowling/BowlingGame.java
+++ src/main/java/kata/bowling/BowlingGame.java
@@ -2,13 +2,24 @@ package kata.bowling;
 
 public class BowlingGame {
 
-    private int score = 0;
+    private final int[] rolls = new int[21];
+    private int rollCounter = 0;
 
     public void score(int pins) {
-        score += pins;
+        rolls[rollCounter] = pins;
+        rollCounter++;
     }
 
     public int getScore() {
+        int score = 0;
+        for (int i = 0; i < 20; i++) {
+            if (rolls[i] + rolls[i+1] == 10) {
+                score += 10 + rolls[i + 2];
+                i++;
+            } else {
+                score += rolls[i];
+            }
+        }
         return score;
     }
 }
