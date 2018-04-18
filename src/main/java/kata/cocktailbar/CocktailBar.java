15eb8544be0098ae9feb72f6a11b8824ccc1acf1
eduardowt <jewt_5@hotmail.com>
2018-04-18 20:42:14 +0100

    writing the code for the fourth AC for cocktail bar kata is relatively straight forward, but how do we prove that it works?

--- src/main/java/kata/cocktailbar/CocktailBar.java
+++ src/main/java/kata/cocktailbar/CocktailBar.java
@@ -1,9 +1,13 @@
 package kata.cocktailbar;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public class CocktailBar {
 
+
+    private static final LocalTime HAPPY_HOUR_START = LocalTime.of(21, 00);
+    private static final LocalTime HAPPY_HOUR_END = LocalTime.of(22, 00);
 
     public int serve(List<Cocktail> orderedCocktails) {
         int totalPrice = 0;
@@ -15,7 +19,13 @@ public class CocktailBar {
                     continue;
                 }
             }
-            totalPrice += orderedCocktail.getPrice();
+
+            LocalTime now = LocalTime.now();
+            if (now.isAfter(HAPPY_HOUR_START) && now.isBefore(HAPPY_HOUR_END)) {
+                totalPrice += orderedCocktail.getPrice() / 2;
+            } else {
+                totalPrice += orderedCocktail.getPrice();
+            }
         }
         return totalPrice;
     }
