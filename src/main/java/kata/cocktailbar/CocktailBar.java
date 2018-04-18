700d796a29771807e0f695d79c9f8ad0c12bdefa
eduardowt <jewt_5@hotmail.com>
2018-04-18 22:07:57 +0100

    voila

--- src/main/java/kata/cocktailbar/CocktailBar.java
+++ src/main/java/kata/cocktailbar/CocktailBar.java
@@ -15,9 +15,6 @@ public class CocktailBar {
     }
 
     public int serve(List<Cocktail> orderedCocktails) {
-        if (orderedCocktails.size() >= 5) {
-            throw new ShouldntGetDrunkException();
-        }
         int totalPrice = 0;
         int oldFashionedsBought = 0;
         for (Cocktail orderedCocktail : orderedCocktails) {
