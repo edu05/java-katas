68bbdaaf22da3f7e642436089b24111d5456e011
eduardowt <jewt_5@hotmail.com>
2018-04-18 22:48:51 +0100

    test fails even if the code is correct, why?

--- src/main/java/kata/amazon/Amazon.java
+++ src/main/java/kata/amazon/Amazon.java
@@ -4,12 +4,15 @@ public class Amazon {
 
 
     private final Warehouse warehouse;
+    private final Wallet wallet;
 
     public Amazon(Warehouse warehouse, Wallet wallet) {
         this.warehouse = warehouse;
+        this.wallet = wallet;
     }
 
     public void buy(Customer customer, Item item) {
+        wallet.bill(customer, item.getPrice());
         warehouse.dispatch(customer, item);
     }
 }
