87eac984084928f9c1e9c418f1312906a992957c
eduardowt <jewt_5@hotmail.com>
2018-04-18 20:31:39 +0100

    code for third AC for cocktail bar kata

--- src/main/java/kata/cocktailbar/CocktailBar.java
+++ src/main/java/kata/cocktailbar/CocktailBar.java
@@ -4,9 +4,17 @@ import java.util.List;
 
 public class CocktailBar {
 
+
     public int serve(List<Cocktail> orderedCocktails) {
         int totalPrice = 0;
+        int oldFashionedsBought = 0;
         for (Cocktail orderedCocktail : orderedCocktails) {
+            if (orderedCocktail.getDescription().equals(Cocktail.oldFashioned().getDescription())) {
+                oldFashionedsBought++;
+                if (oldFashionedsBought % 2 == 0) {
+                    continue;
+                }
+            }
             totalPrice += orderedCocktail.getPrice();
         }
         return totalPrice;
