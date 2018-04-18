e17fb13cda29e795e33207af07d5156cd4214fd6
eduardowt <jewt_5@hotmail.com>
2018-04-18 21:08:02 +0100

    same tests with mocks, now we don't need the extra AlwaysHappyHourClock and the NeverHappyHourClock classes

--- src/test/java/kata/cocktailbar/CocktailBarTest.java
+++ src/test/java/kata/cocktailbar/CocktailBarTest.java
@@ -2,25 +2,30 @@ package kata.cocktailbar;
 
 import org.junit.Test;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CocktailBarTest {
 
-    private Clock clock = new NeverHappyHourClock();
+    private Clock clock = mock(Clock.class);
 
     @Test
     public void testEmptyOrderCostsNothing() throws Exception {
+        when(clock.getTime()).thenReturn(LocalTime.of(13,00));
         CocktailBar cocktailBar = new CocktailBar(clock);
         assertThat(cocktailBar.serve(new ArrayList<>()), is(0));
     }
 
     @Test
     public void testAddingPrices() throws Exception {
+        when(clock.getTime()).thenReturn(LocalTime.of(13,00));
         CocktailBar cocktailBar = new CocktailBar(clock);
         List<Cocktail> cocktails = asList(Cocktail.mojito(), Cocktail.bloodyMary());
 
@@ -29,6 +34,7 @@ public class CocktailBarTest {
 
     @Test
     public void testPriceForSingleOldFashioned() throws Exception {
+        when(clock.getTime()).thenReturn(LocalTime.of(13,00));
         CocktailBar cocktailBar = new CocktailBar(clock);
         List<Cocktail> cocktails = asList(Cocktail.oldFashioned());
 
@@ -37,6 +43,7 @@ public class CocktailBarTest {
 
     @Test
     public void testPriceForTwoOldFashioned() throws Exception {
+        when(clock.getTime()).thenReturn(LocalTime.of(13,00));
         CocktailBar cocktailBar = new CocktailBar(clock);
         List<Cocktail> cocktails = asList(Cocktail.oldFashioned(), Cocktail.oldFashioned());
 
@@ -51,5 +58,16 @@ public class CocktailBarTest {
 
         assertThat(cocktailBar.serve(cocktails), is(5));
 
+    }
+
+    @Test
+    public void testHappyHourWithMocks() throws Exception {
+        Clock fakeClock = mock(Clock.class);
+        when(fakeClock.getTime()).thenReturn(LocalTime.of(21, 30));
+        CocktailBar cocktailBar = new CocktailBar(fakeClock);
+
+        List<Cocktail> cocktails = asList(Cocktail.mojito());
+
+        assertThat(cocktailBar.serve(cocktails), is(5));
     }
 }
