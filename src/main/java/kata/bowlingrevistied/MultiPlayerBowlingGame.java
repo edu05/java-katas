acda72c8172b04fc57d846fd228a2e3c20d5ca5b
eduardowt <jewt_5@hotmail.com>
2018-05-01 15:31:12 +0100

    new kata - multiplayer bowling game given the code for a single player bowling game

---
+++ src/main/java/kata/bowlingrevistied/MultiPlayerBowlingGame.java
@@ -0,0 +1,29 @@
+package kata.bowlingrevistied;
+
+public class MultiPlayerBowlingGame {
+
+    private final int[] rolls = new int[21];
+    private int rollCounter = 0;
+
+    public void roll(int pins) {
+        rolls[rollCounter] = pins;
+        rollCounter++;
+    }
+
+    public int getScore() {
+        int score = 0;
+        int rollsLeft = 20;
+        for (int i = 0; i < rollsLeft; i++) {
+            if (rolls[i] == 10) {
+                score += 10 + rolls[i + 1] + rolls[i + 2];
+                rollsLeft--;
+            } else if (rolls[i] + rolls[i + 1] == 10) {
+                score += 10 + rolls[i + 2];
+                i++;
+            } else {
+                score += rolls[i];
+            }
+        }
+        return score;
+    }
+}
---
+++ src/main/java/kata/bowlingrevistied/README.md
@@ -0,0 +1,3 @@
+# multiplayer bowling game kata
+
+The goal of this kata is to reinforce your knowledge on some of Java's common data structures while practicing TDD. For this we'll re-use the code from a previous exercise.
