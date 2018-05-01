172da08d8a9b7637c0e182eecf79aab5f1827fa8
eduardowt <jewt_5@hotmail.com>
2018-05-01 16:33:44 +0100

    our previous change made our test pass, so we need to break it for it to be a relevant test

--- src/test/java/kata/bowlingrevistied/MultiPlayerBowlingGameTest.java
+++ src/test/java/kata/bowlingrevistied/MultiPlayerBowlingGameTest.java
@@ -87,11 +87,11 @@ public class MultiPlayerBowlingGameTest {
         MultiPlayerBowlingGame multiPlayerBowlingGame = new MultiPlayerBowlingGame(Arrays.asList(NADAL, FEDERER));
 
         for (int i = 0; i < 20; i++) {
-            multiPlayerBowlingGame.roll(1, NADAL);
+            multiPlayerBowlingGame.roll(2, NADAL);
             multiPlayerBowlingGame.roll(1, FEDERER);
         }
 
-        assertThat(multiPlayerBowlingGame.getScore(NADAL), is(20));
+        assertThat(multiPlayerBowlingGame.getScore(NADAL), is(40));
         assertThat(multiPlayerBowlingGame.getScore(FEDERER), is(20));
     }
 }
