2245cfa46dc213a73b1d1e9f2c24dd8fba0d4b02
eduardowt <jewt_5@hotmail.com>
2018-05-01 15:52:27 +0100

    because I'm going to do some refactoring before making the changes for the multiplayer bowling game, I want to make sure I don't break any tests. they will protect me from doing bad changes

--- src/test/java/kata/bowlingrevistied/MultiPlayerBowlingGameTest.java
+++ src/test/java/kata/bowlingrevistied/MultiPlayerBowlingGameTest.java
@@ -5,12 +5,82 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class MultiPlayerBowlingGameTest {
 
     private static final String NADAL = "Nadal";
     private static final String FEDERER = "Federer";
+
+    @Test
+    public void testGutterGame() throws Exception {
+        MultiPlayerBowlingGame bowlingGame = new MultiPlayerBowlingGame(Arrays.asList(NADAL));
+
+
+        scoreNTimes(bowlingGame, 0, 20);
+
+        assertThat(bowlingGame.getScore(NADAL), is(0));
+    }
+
+    @Test
+    public void testSimpleGame() throws Exception {
+        MultiPlayerBowlingGame bowlingGame = new MultiPlayerBowlingGame(Arrays.asList(NADAL));
+
+
+        scoreNTimes(bowlingGame, 1, 20);
+
+        assertThat(bowlingGame.getScore(NADAL), is(20));
+    }
+
+    @Test
+    public void testSpareGame() throws Exception {
+        MultiPlayerBowlingGame bowlingGame = new MultiPlayerBowlingGame(Arrays.asList(NADAL));
+
+
+        scoreNTimes(bowlingGame, 5, 2);
+        scoreNTimes(bowlingGame, 1, 18);
+
+        assertThat(bowlingGame.getScore(NADAL), is(29));
+    }
+
+    @Test
+    public void testSpareInFinalFrame() throws Exception {
+        MultiPlayerBowlingGame bowlingGame = new MultiPlayerBowlingGame(Arrays.asList(NADAL));
+
+
+        scoreNTimes(bowlingGame, 1, 18);
+        scoreNTimes(bowlingGame, 5, 2);
+        scoreNTimes(bowlingGame, 1, 1);
+
+        assertThat(bowlingGame.getScore(NADAL), is(29));
+    }
+
+    @Test
+    public void testStrike() throws Exception {
+        MultiPlayerBowlingGame bowlingGame = new MultiPlayerBowlingGame(Arrays.asList(NADAL));
+
+
+        scoreNTimes(bowlingGame, 10, 1);
+        scoreNTimes(bowlingGame, 1, 18);
+
+        assertThat(bowlingGame.getScore(NADAL), is(30));
+    }
+
+    @Test
+    public void testMaxScore() throws Exception {
+        MultiPlayerBowlingGame bowlingGame = new MultiPlayerBowlingGame(Arrays.asList(NADAL));
+
+
+        scoreNTimes(bowlingGame, 10, 12);
+
+        assertThat(bowlingGame.getScore(NADAL), is(300));
+    }
+
+    private void scoreNTimes(MultiPlayerBowlingGame bowlingGame, int pins, int nTimes) {
+        for (int i = 0; i < nTimes; i++) {
+            bowlingGame.roll(pins, NADAL);
+        }
+    }
 
     @Test
     public void testTwoPlayersScoring() throws Exception {
