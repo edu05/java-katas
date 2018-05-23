e3a059f5f6419acafed901a4026dfec3b26460e6
eduardowt <jewt_5@hotmail.com>
2018-05-23 20:49:53 +0100

    code for first AC

--- src/main/java/kata/mp3/MP3Player.java
+++ src/main/java/kata/mp3/MP3Player.java
@@ -5,12 +5,22 @@ import java.util.List;
 
 public class MP3Player {
 
+    private final List<Song> songs = new ArrayList<>();
+
     public void addSong(Song song) {
+        songs.add(song);
 
     }
 
     public List<Song> findByTitle(String songTitle) {
-        return new ArrayList<>();
+        List<Song> matchingSongs = new ArrayList<>();
+        for (Song song : songs) {
+            if (song.getTitle().equals(songTitle)) {
+                matchingSongs.add(song);
+            }
+        }
+
+        return matchingSongs;
     }
 
 
