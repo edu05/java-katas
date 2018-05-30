6dd9740a5e8f84d9c1060f35252070a80c0b7a56
eduardowt <jewt_5@hotmail.com>
2018-05-30 20:52:39 +0100

    code for first AC

--- src/main/java/kata/lrucache/Cache.java
+++ src/main/java/kata/lrucache/Cache.java
@@ -1,13 +1,17 @@
 package kata.lrucache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cache {
 
+    private final Map<Object, Object> cache = new HashMap<>();
 
     public void put(Object key, Object value) {
-
+        cache.put(key, value);
     }
 
     public Object get(Object key) {
-        return null;
+        return cache.get(key);
     }
 }
