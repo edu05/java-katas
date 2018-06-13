a4d79474eb6de3f0268756d0dfc5330d3c21c4da
eduardowt <jewt_5@hotmail.com>
2018-06-13 22:00:11 +0100

    code for first AC on exception kata *notice the ArXivRepository's method declares it throws an IOException that we need to either catch or re-throw, which should we choose?

--- src/main/java/kata/exceptions/ScientificPaperAggregator.java
+++ src/main/java/kata/exceptions/ScientificPaperAggregator.java
@@ -1,13 +1,35 @@
 package kata.exceptions;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScientificPaperAggregator {
 
+    private final ArXivRepository arXivRepository;
+    private final JSTORRepository jstorRepository;
+    private final MobileReaderApp mobileReaderApp;
 
     public ScientificPaperAggregator(ArXivRepository arXivRepository, JSTORRepository jstorRepository, MobileReaderApp mobileReaderApp) {
+        this.arXivRepository = arXivRepository;
+        this.jstorRepository = jstorRepository;
 
+        this.mobileReaderApp = mobileReaderApp;
     }
 
     public void aggregateNewScientificPapers() {
+        List<ScientificPaper> newScientificPapersFromArxiv = new ArrayList<>();
+        try {
+            newScientificPapersFromArxiv = arXivRepository.getNewScientificPapers();
+        } catch (IOException e) {
+            //what should we do here?
+        }
 
+        List<ScientificPaper> newScientificPapersFromJSTOR = jstorRepository.getNewScientificPapers();
+
+        List<ScientificPaper> aggregatedNewScientificPapers = new ArrayList<>();
+        aggregatedNewScientificPapers.addAll(newScientificPapersFromArxiv);
+        aggregatedNewScientificPapers.addAll(newScientificPapersFromJSTOR);
+        mobileReaderApp.pushContent(aggregatedNewScientificPapers);
     }
 }
