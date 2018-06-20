7e9c408506965e38d7852a491c2af0966141325a
eduardowt <jewt_5@hotmail.com>
2018-06-20 22:42:04 +0100

    code for test for fourth AC

--- src/main/java/kata/surfing/SurfingApp.java
+++ src/main/java/kata/surfing/SurfingApp.java
@@ -2,15 +2,16 @@ package kata.surfing;
 
 public class SurfingApp {
 
-    private static final int WAIKIKI_CITY_ID = 55;
     private final WeatherApp weatherApp;
     private final NotifierApp notifierApp;
     private final ClockApp clockApp;
+    private final GPSApp gpsApp;
 
     public SurfingApp(WeatherApp weatherApp, NotifierApp notifierApp, ClockApp clockApp, GPSApp gpsApp) {
         this.weatherApp = weatherApp;
         this.notifierApp = notifierApp;
         this.clockApp = clockApp;
+        this.gpsApp = gpsApp;
     }
 
     public void alertOnGoodWeatherConditions() {
@@ -18,14 +19,19 @@ public class SurfingApp {
             return;
         }
 
-        WeatherConditions weatherConditions = weatherApp.getWeatherConditions(WAIKIKI_CITY_ID);
+        gpsApp.getCitiesNearby().stream()
+                .filter(City::hasBeach)
+                .forEach(city -> {
+                    WeatherConditions weatherConditions = weatherApp.getWeatherConditions(city.getCityId());
 
-        int temperature = weatherConditions.getTemperatureInCelsius();
-        int windSpeed = weatherConditions.getWindSpeedInMetersPerHour();
-        if (temperature > 25 && windSpeed > 12) {
-            notifierApp.notifyAndVibrate("Go surfing ASAP!");
-        } else if ((temperature >= 21 && windSpeed >= 12) || (temperature >= 25 && windSpeed >= 9)) {
-            notifierApp.notify("You should go surfing today");
-        }
+                    int temperature = weatherConditions.getTemperatureInCelsius();
+                    int windSpeed = weatherConditions.getWindSpeedInMetersPerHour();
+                    if (temperature > 25 && windSpeed > 12) {
+                        notifierApp.notifyAndVibrate("Go surfing ASAP to " + city.getCityName() + "!");
+                    } else if ((temperature >= 21 && windSpeed >= 12) || (temperature >= 25 && windSpeed >= 9)) {
+                        notifierApp.notify("You should go surfing today to " + city.getCityName());
+                    }
+
+                });
     }
 }
