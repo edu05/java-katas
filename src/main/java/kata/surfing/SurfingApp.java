02ff64950c22f7637aa055fea2e1efc8594e2176
eduardowt <jewt_5@hotmail.com>
2018-06-20 22:10:01 +0100

    code for test for third AC

--- src/main/java/kata/surfing/SurfingApp.java
+++ src/main/java/kata/surfing/SurfingApp.java
@@ -5,13 +5,19 @@ public class SurfingApp {
     private static final int WAIKIKI_CITY_ID = 55;
     private final WeatherApp weatherApp;
     private final NotifierApp notifierApp;
+    private final ClockApp clockApp;
 
     public SurfingApp(WeatherApp weatherApp, NotifierApp notifierApp, ClockApp clockApp) {
         this.weatherApp = weatherApp;
         this.notifierApp = notifierApp;
+        this.clockApp = clockApp;
     }
 
     public void alertOnGoodWeatherConditions() {
+        if (clockApp.getCurrentTime().getMinute() != 0) {
+            return;
+        }
+
         WeatherConditions weatherConditions = weatherApp.getWeatherConditions(WAIKIKI_CITY_ID);
 
         int temperature = weatherConditions.getTemperatureInCelsius();
