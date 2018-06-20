316a881604e6f25afbdc63ead0187b5defe148f2
eduardowt <jewt_5@hotmail.com>
2018-06-20 21:59:20 +0100

    code for tests for first AC

--- src/main/java/kata/surfing/SurfingApp.java
+++ src/main/java/kata/surfing/SurfingApp.java
@@ -2,11 +2,22 @@ package kata.surfing;
 
 public class SurfingApp {
 
-    public SurfingApp(WeatherApp weatherApp, NotifierApp notifierApp) {
+    private static final int WAIKIKI_CITY_ID = 55;
+    private final WeatherApp weatherApp;
+    private final NotifierApp notifierApp;
 
+    public SurfingApp(WeatherApp weatherApp, NotifierApp notifierApp) {
+        this.weatherApp = weatherApp;
+        this.notifierApp = notifierApp;
     }
 
     public void alertOnGoodWeatherConditions() {
+        WeatherConditions weatherConditions = weatherApp.getWeatherConditions(WAIKIKI_CITY_ID);
 
+        int temperature = weatherConditions.getTemperatureInCelsius();
+        int windSpeed = weatherConditions.getWindSpeedInMetersPerHour();
+        if ((temperature >= 21 && windSpeed >= 12) || (temperature >= 25 && windSpeed >= 9)) {
+            notifierApp.notify("You should go surfing today");
+        }
     }
 }
