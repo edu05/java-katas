d30d9fdf5d267e6de65ac48fc120a37052071bed
eduardowt <jewt_5@hotmail.com>
2018-07-25 00:21:34 +0100

    working code for the first AC of the calendar kata

--- src/main/java/kata/calendar/CalendarManager.java
+++ src/main/java/kata/calendar/CalendarManager.java
@@ -2,14 +2,19 @@ package kata.calendar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CalendarManager {
 
+    private List<Meeting> meetings = new ArrayList<>();
 
     public void schedule(Meeting newMeeting) {
+        meetings.add(newMeeting);
     }
 
     public List<Meeting> scheduledMeetings() {
-        return new ArrayList<>();
+        return meetings.stream()
+                .sorted((lhs, rhs) -> lhs.getStart().isBefore(rhs.getStart()) ? -1 : 1)
+                .collect(Collectors.toList());
     }
 }
