fa286f1c3e73e00f70dce398b17e8387bffb66dd
eduardowt <jewt_5@hotmail.com>
2018-07-25 00:31:28 +0100

    test for the second AC of the calendar kata

--- src/test/java/kata/calendar/CalendarManagerTest.java
+++ src/test/java/kata/calendar/CalendarManagerTest.java
@@ -1,5 +1,6 @@
 package kata.calendar;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
@@ -10,7 +11,13 @@ import static org.junit.Assert.assertThat;
 
 public class CalendarManagerTest {
 
-    private CalendarManager calendarManager = new CalendarManager();
+    private CalendarManager calendarManager;
+
+    @Before
+    public void setUp() throws Exception {
+        //empty out the calendar with each test run
+        calendarManager = new CalendarManager();
+    }
 
     @Test
     public void shouldScheduleMeetingsAndGetThemInOrder() throws Exception {
@@ -25,5 +32,91 @@ public class CalendarManagerTest {
         assertThat(scheduledMeetings.size(), is(2));
         assertThat(scheduledMeetings.get(0), is(meeting));
         assertThat(scheduledMeetings.get(1), is(anotherMeeting));
+    }
+
+    @Test
+    public void shouldNotScheduleMeetingOverlappingEndTime() throws Exception {
+        Meeting meeting = new Meeting("meeting", LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        Meeting meetingOverlappingEndTime = new Meeting("meetingOverlappingEndTime", LocalDateTime.now().plusMinutes(30), LocalDateTime.now().plusHours(3));
+
+        calendarManager.schedule(meeting);
+        calendarManager.schedule(meetingOverlappingEndTime);
+
+        List<Meeting> scheduledMeetings = calendarManager.scheduledMeetings();
+
+        assertThat(scheduledMeetings.size(), is(1));
+        assertThat(scheduledMeetings.get(0), is(meeting));
+    }
+
+    @Test
+    public void shouldNotScheduleMeetingOverlappingStartTime() throws Exception {
+        Meeting meeting = new Meeting("meeting", LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        Meeting meetingOverlappingStartTime = new Meeting("meetingOverlappingStartTime", LocalDateTime.now().minusMinutes(30), LocalDateTime.now().plusMinutes(1));
+
+        calendarManager.schedule(meeting);
+        calendarManager.schedule(meetingOverlappingStartTime);
+
+        List<Meeting> scheduledMeetings = calendarManager.scheduledMeetings();
+
+        assertThat(scheduledMeetings.size(), is(1));
+        assertThat(scheduledMeetings.get(0), is(meeting));
+
+    }
+
+    @Test
+    public void shouldNotScheduleMeetingOverlappingFromTheInside() throws Exception {
+        Meeting meeting = new Meeting("meeting", LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        Meeting meetingOverlappingFromTheInside = new Meeting("meetingOverlappingFromTheInside", LocalDateTime.now().plusMinutes(30), LocalDateTime.now().plusMinutes(31));
+
+        calendarManager.schedule(meeting);
+        calendarManager.schedule(meetingOverlappingFromTheInside);
+
+        List<Meeting> scheduledMeetings = calendarManager.scheduledMeetings();
+
+        assertThat(scheduledMeetings.size(), is(1));
+        assertThat(scheduledMeetings.get(0), is(meeting));
+
+
+    }
+
+    @Test
+    public void shouldNotScheduleFullyOverlappingMeeting() throws Exception {
+        Meeting meeting = new Meeting("meeting", LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        Meeting fullyOverlappingMeeting = new Meeting("fullyOverlappingMeeting", LocalDateTime.now().minusMinutes(30), LocalDateTime.now().plusHours(3));
+
+        calendarManager.schedule(meeting);
+        calendarManager.schedule(fullyOverlappingMeeting);
+
+        List<Meeting> scheduledMeetings = calendarManager.scheduledMeetings();
+
+        assertThat(scheduledMeetings.size(), is(1));
+        assertThat(scheduledMeetings.get(0), is(meeting));
+
+    }
+
+    @Test
+    public void shouldScheduleMeetingWhenEndTimeIsBeforeAnyStartTime() throws Exception {
+        Meeting meeting = new Meeting("meeting", LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        Meeting earlierMeeting = new Meeting("earlier meeting", LocalDateTime.now().minusMinutes(30), LocalDateTime.now().minusMinutes(10));
+
+        calendarManager.schedule(meeting);
+        calendarManager.schedule(earlierMeeting);
+
+        List<Meeting> scheduledMeetings = calendarManager.scheduledMeetings();
+
+        assertThat(scheduledMeetings.size(), is(2));
+    }
+
+    @Test
+    public void shouldScheduleMeetingWhenStartTimeIsAfterAnyEndTime() throws Exception {
+        Meeting meeting = new Meeting("meeting", LocalDateTime.now(), LocalDateTime.now().plusHours(1));
+        Meeting laterMeeting = new Meeting("later meeting", LocalDateTime.now().plusHours(2), LocalDateTime.now().plusHours(3));
+
+        calendarManager.schedule(meeting);
+        calendarManager.schedule(laterMeeting);
+
+        List<Meeting> scheduledMeetings = calendarManager.scheduledMeetings();
+
+        assertThat(scheduledMeetings.size(), is(2));
     }
 }
