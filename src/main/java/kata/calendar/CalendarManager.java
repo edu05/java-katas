a1e510f95a03d15a2a825e0dd4eb4c8565ea2133
eduardowt <jewt_5@hotmail.com>
2018-07-25 01:01:20 +0100

    6th part of the kata

--- src/main/java/kata/calendar/CalendarManager.java
+++ src/main/java/kata/calendar/CalendarManager.java
@@ -2,6 +2,7 @@ package kata.calendar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CalendarManager {
@@ -9,16 +10,25 @@ public class CalendarManager {
     private List<Meeting> meetings = new ArrayList<>();
 
     public CalendarOperationResult schedule(Meeting newMeeting) {
-        for (Meeting meeting : meetings) {
-            if (meeting.getName().equals(newMeeting.getName())) {
-                return CalendarOperationResult.failure("existing meeting with same name " + meeting);
-            } else if (!newMeeting.getEnd().isBefore(meeting.getStart()) && !newMeeting.getStart().isAfter(meeting.getEnd())) {
-                return CalendarOperationResult.failure("clashing with meeting " + meeting);
-            }
-        }
+        Optional<CalendarOperationResult> failedCalendarOperationResult = meetings.stream()
+                .map(meeting -> {
+                    if (meeting.getName().equals(newMeeting.getName())) {
+                        return CalendarOperationResult.failure("existing meeting with same name " + meeting);
+                    } else if (!newMeeting.getEnd().isBefore(meeting.getStart()) && !newMeeting.getStart().isAfter(meeting.getEnd())) {
+                        return CalendarOperationResult.failure("clashing with meeting " + meeting);
+                    } else {
+                        return CalendarOperationResult.success();
+                    }
+                })
+                .filter(calendarOperationResult -> calendarOperationResult.isFailure())
+                .findFirst();
 
-        meetings.add(newMeeting);
-        return CalendarOperationResult.success();
+        if (failedCalendarOperationResult.isPresent()) {
+            return failedCalendarOperationResult.get();
+        } else {
+            meetings.add(newMeeting);
+            return CalendarOperationResult.success();
+        }
     }
 
     public List<Meeting> scheduledMeetings() {
